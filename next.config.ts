d656aedf90f439cd8df21fccddac83fec2948eff
Bruno Henrique Cordeiro <bruno.cordeiroh@gmail.com>
2025-06-18 16:53:23 +0000

    Error: An unexpected Turbopack error occurred. Please see the output of

--- next.config.ts
+++ next.config.ts
@@ -9,16 +9,16 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'placehold.co',
+  //       port: '',
+  //       pathname: '/**',
+  //     },
+  //   ],
+  // },
 };
 
 export default nextConfig;

aab5cf6778df431f04982460279fecd77baeee08
Bruno Henrique Cordeiro <bruno.cordeiroh@gmail.com>
2025-06-18 17:08:09 +0000

    Error: An unexpected Turbopack error occurred. Please see the output of

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,12 @@ import type { NextConfig } from 'next';
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
   // images: {
   //   remotePatterns: [
   //     {

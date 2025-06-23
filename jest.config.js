104bd14e235fa56f4b637f0fa6df235c5c412a2f
brunenrique <93277400+brunenrique@users.noreply.github.com>
2025-06-23 18:19:00 -0300

    feat: add example test and jest config

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+const nextJest = require('next/jest')
+const createJestConfig = nextJest({ dir: './' })
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/setupTests.ts', '<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jsdom',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['html', 'text'],
+  coverageThreshold: {
+    global: {
+      branches: 85,
+      functions: 85,
+      lines: 85,
+      statements: 85,
+    },
+  },
+}
+
+module.exports = createJestConfig(customJestConfig)
---
+++ tests/example.test.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect } from '@jest/globals'
+
+describe('sanity check', () => {
+  it('true is true', () => {
+    expect(true).toBe(true)
+  })
+})

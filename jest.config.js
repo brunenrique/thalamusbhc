a1c3cd0bfd6e97e654d49d8c656354aaf1871f1e
brunenrique <93277400+brunenrique@users.noreply.github.com>
2025-06-23 09:32:00 -0300

    feat(e2e): testes de agendamento e notificacoes

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,17 @@ const createJestConfig = nextJest({ dir: './' })
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
   testEnvironment: 'jsdom',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['html', 'text'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 }
 
 module.exports = createJestConfig(customJestConfig)

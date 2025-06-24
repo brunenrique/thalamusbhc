ea137bb556cdf5e42fafa148a754deb484542265
brunenrique <93277400+brunenrique@users.noreply.github.com>
2025-06-24 00:32:25 -0300

    fix: update firebase emulator ports

--- __tests__/groups.service.test.ts
+++ __tests__/groups.service.test.ts
@@ -10,7 +10,7 @@ let deleteGroup: any;
 let testEnv: Awaited<ReturnType<typeof initializeTestEnvironment>>;
 
 beforeAll(async () => {
-  const hostPort = process.env.FIRESTORE_EMULATOR_HOST || '127.0.0.1:8084';
+  const hostPort = process.env.FIRESTORE_EMULATOR_HOST || '127.0.0.1:8085';
   const [host, portStr] = hostPort.split(':');
   const port = parseInt(portStr, 10);
 
